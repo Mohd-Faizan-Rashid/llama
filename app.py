71dd04d1fd08848931ef3bc3f5843ce85d3292aa
mohdfaizan <a-9739@kmclu.ac.in>
2025-02-14 20:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def process_request():
 import os
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    PORT = int(os.environ.get("PORT", 10000))  # Default to 5000 if PORT not set
     app.run(host="0.0.0.0", port=PORT)
